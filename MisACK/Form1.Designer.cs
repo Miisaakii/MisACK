976073b1622e9196c98fffcab31f3136b713cc17
Misakiii <59036402+Miisaakii@users.noreply.github.com>
2023-03-02 01:00:19 +0100

    Add files via upload

---
+++ MisACK/Form1.Designer.cs
@@ -0,0 +1,262 @@
+﻿namespace MisACK
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur Windows Form
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.guna2Elipse1 = new Guna.UI2.WinForms.Guna2Elipse(this.components);
+            this.gunaGradient2Panel1 = new Guna.UI.WinForms.GunaGradient2Panel();
+            this.gunaLabel1 = new Guna.UI.WinForms.GunaLabel();
+            this.guna2ImageButton1 = new Guna.UI2.WinForms.Guna2ImageButton();
+            this.guna2TextBox1 = new Guna.UI2.WinForms.Guna2TextBox();
+            this.guna2Button1 = new Guna.UI2.WinForms.Guna2Button();
+            this.guna2Button2 = new Guna.UI2.WinForms.Guna2Button();
+            this.guna2CheckBox1 = new Guna.UI2.WinForms.Guna2CheckBox();
+            this.gunaComboBox1 = new Guna.UI.WinForms.GunaComboBox();
+            this.gunaComboBox2 = new Guna.UI.WinForms.GunaComboBox();
+            this.gunaGradient2Panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // guna2Elipse1
+            // 
+            this.guna2Elipse1.TargetControl = this;
+            // 
+            // gunaGradient2Panel1
+            // 
+            this.gunaGradient2Panel1.BackColor = System.Drawing.Color.Transparent;
+            this.gunaGradient2Panel1.Controls.Add(this.gunaLabel1);
+            this.gunaGradient2Panel1.Controls.Add(this.guna2ImageButton1);
+            this.gunaGradient2Panel1.GradientColor1 = System.Drawing.Color.DarkOrchid;
+            this.gunaGradient2Panel1.GradientColor2 = System.Drawing.Color.Crimson;
+            this.gunaGradient2Panel1.Location = new System.Drawing.Point(0, -1);
+            this.gunaGradient2Panel1.Name = "gunaGradient2Panel1";
+            this.gunaGradient2Panel1.Size = new System.Drawing.Size(155, 21);
+            this.gunaGradient2Panel1.TabIndex = 0;
+            // 
+            // gunaLabel1
+            // 
+            this.gunaLabel1.AutoSize = true;
+            this.gunaLabel1.Font = new System.Drawing.Font("Segoe UI", 9F);
+            this.gunaLabel1.ForeColor = System.Drawing.Color.White;
+            this.gunaLabel1.Location = new System.Drawing.Point(6, 4);
+            this.gunaLabel1.Name = "gunaLabel1";
+            this.gunaLabel1.Size = new System.Drawing.Size(49, 15);
+            this.gunaLabel1.TabIndex = 1;
+            this.gunaLabel1.Text = "MisACK";
+            // 
+            // guna2ImageButton1
+            // 
+            this.guna2ImageButton1.CheckedState.Parent = this.guna2ImageButton1;
+            this.guna2ImageButton1.HoverState.Parent = this.guna2ImageButton1;
+            this.guna2ImageButton1.Image = ((System.Drawing.Image)(resources.GetObject("guna2ImageButton1.Image")));
+            this.guna2ImageButton1.ImageSize = new System.Drawing.Size(16, 16);
+            this.guna2ImageButton1.Location = new System.Drawing.Point(133, 3);
+            this.guna2ImageButton1.Name = "guna2ImageButton1";
+            this.guna2ImageButton1.PressedState.Parent = this.guna2ImageButton1;
+            this.guna2ImageButton1.Size = new System.Drawing.Size(16, 16);
+            this.guna2ImageButton1.TabIndex = 1;
+            this.guna2ImageButton1.Click += new System.EventHandler(this.guna2ImageButton1_Click);
+            // 
+            // guna2TextBox1
+            // 
+            this.guna2TextBox1.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(12)))), ((int)(((byte)(12)))), ((int)(((byte)(12)))));
+            this.guna2TextBox1.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.guna2TextBox1.DefaultText = "";
+            this.guna2TextBox1.DisabledState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(208)))), ((int)(((byte)(208)))), ((int)(((byte)(208)))));
+            this.guna2TextBox1.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(226)))), ((int)(((byte)(226)))), ((int)(((byte)(226)))));
+            this.guna2TextBox1.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(138)))), ((int)(((byte)(138)))), ((int)(((byte)(138)))));
+            this.guna2TextBox1.DisabledState.Parent = this.guna2TextBox1;
+            this.guna2TextBox1.DisabledState.PlaceholderForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(138)))), ((int)(((byte)(138)))), ((int)(((byte)(138)))));
+            this.guna2TextBox1.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(32)))), ((int)(((byte)(32)))), ((int)(((byte)(32)))));
+            this.guna2TextBox1.FocusedState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
+            this.guna2TextBox1.FocusedState.Parent = this.guna2TextBox1;
+            this.guna2TextBox1.HoverState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
+            this.guna2TextBox1.HoverState.Parent = this.guna2TextBox1;
+            this.guna2TextBox1.Location = new System.Drawing.Point(11, 112);
+            this.guna2TextBox1.Name = "guna2TextBox1";
+            this.guna2TextBox1.PasswordChar = '\0';
+            this.guna2TextBox1.PlaceholderText = "Timer Lag";
+            this.guna2TextBox1.SelectedText = "";
+            this.guna2TextBox1.ShadowDecoration.Parent = this.guna2TextBox1;
+            this.guna2TextBox1.Size = new System.Drawing.Size(133, 21);
+            this.guna2TextBox1.TabIndex = 1;
+            // 
+            // guna2Button1
+            // 
+            this.guna2Button1.CheckedState.Parent = this.guna2Button1;
+            this.guna2Button1.CustomImages.Parent = this.guna2Button1;
+            this.guna2Button1.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(25)))), ((int)(((byte)(25)))), ((int)(((byte)(25)))));
+            this.guna2Button1.Font = new System.Drawing.Font("Segoe UI", 9F);
+            this.guna2Button1.ForeColor = System.Drawing.Color.White;
+            this.guna2Button1.HoverState.Parent = this.guna2Button1;
+            this.guna2Button1.Location = new System.Drawing.Point(11, 139);
+            this.guna2Button1.Name = "guna2Button1";
+            this.guna2Button1.ShadowDecoration.Parent = this.guna2Button1;
+            this.guna2Button1.Size = new System.Drawing.Size(133, 21);
+            this.guna2Button1.TabIndex = 2;
+            this.guna2Button1.Text = "Set timer";
+            this.guna2Button1.Click += new System.EventHandler(this.guna2Button1_Click);
+            // 
+            // guna2Button2
+            // 
+            this.guna2Button2.CheckedState.Parent = this.guna2Button2;
+            this.guna2Button2.CustomImages.Parent = this.guna2Button2;
+            this.guna2Button2.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(25)))), ((int)(((byte)(25)))), ((int)(((byte)(25)))));
+            this.guna2Button2.Font = new System.Drawing.Font("Segoe UI", 9F);
+            this.guna2Button2.ForeColor = System.Drawing.Color.White;
+            this.guna2Button2.HoverState.Parent = this.guna2Button2;
+            this.guna2Button2.Location = new System.Drawing.Point(11, 31);
+            this.guna2Button2.Name = "guna2Button2";
+            this.guna2Button2.ShadowDecoration.Parent = this.guna2Button2;
+            this.guna2Button2.Size = new System.Drawing.Size(133, 21);
+            this.guna2Button2.TabIndex = 3;
+            this.guna2Button2.Text = "Select Program";
+            this.guna2Button2.Click += new System.EventHandler(this.guna2Button2_Click);
+            // 
+            // guna2CheckBox1
+            // 
+            this.guna2CheckBox1.AutoSize = true;
+            this.guna2CheckBox1.CheckedState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
+            this.guna2CheckBox1.CheckedState.BorderRadius = 2;
+            this.guna2CheckBox1.CheckedState.BorderThickness = 0;
+            this.guna2CheckBox1.CheckedState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
+            this.guna2CheckBox1.Font = new System.Drawing.Font("Segoe UI", 9F);
+            this.guna2CheckBox1.ForeColor = System.Drawing.Color.White;
+            this.guna2CheckBox1.Location = new System.Drawing.Point(39, 166);
+            this.guna2CheckBox1.Name = "guna2CheckBox1";
+            this.guna2CheckBox1.Size = new System.Drawing.Size(77, 19);
+            this.guna2CheckBox1.TabIndex = 4;
+            this.guna2CheckBox1.Text = "Start loop";
+            this.guna2CheckBox1.UncheckedState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(125)))), ((int)(((byte)(137)))), ((int)(((byte)(149)))));
+            this.guna2CheckBox1.UncheckedState.BorderRadius = 2;
+            this.guna2CheckBox1.UncheckedState.BorderThickness = 0;
+            this.guna2CheckBox1.UncheckedState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(125)))), ((int)(((byte)(137)))), ((int)(((byte)(149)))));
+            this.guna2CheckBox1.UseVisualStyleBackColor = true;
+            this.guna2CheckBox1.CheckedChanged += new System.EventHandler(this.guna2CheckBox1_CheckedChanged);
+            // 
+            // gunaComboBox1
+            // 
+            this.gunaComboBox1.BackColor = System.Drawing.Color.Transparent;
+            this.gunaComboBox1.BaseColor = System.Drawing.Color.FromArgb(((int)(((byte)(25)))), ((int)(((byte)(25)))), ((int)(((byte)(25)))));
+            this.gunaComboBox1.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(12)))), ((int)(((byte)(12)))), ((int)(((byte)(12)))));
+            this.gunaComboBox1.BorderSize = 1;
+            this.gunaComboBox1.DrawMode = System.Windows.Forms.DrawMode.OwnerDrawFixed;
+            this.gunaComboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.gunaComboBox1.FocusedColor = System.Drawing.Color.Empty;
+            this.gunaComboBox1.Font = new System.Drawing.Font("Segoe UI", 9F);
+            this.gunaComboBox1.ForeColor = System.Drawing.Color.White;
+            this.gunaComboBox1.FormattingEnabled = true;
+            this.gunaComboBox1.ItemHeight = 15;
+            this.gunaComboBox1.Items.AddRange(new object[] {
+            "Left mouse button",
+            "Right mouse button",
+            "Middle mouse button",
+            "X1 mouse button",
+            "X2 mouse button",
+            "Shift Key",
+            "CTRL Key",
+            "ALT Key",
+            "A Key",
+            "F Key",
+            "F1 Key",
+            "F2 Key",
+            "F3 Key"});
+            this.gunaComboBox1.Location = new System.Drawing.Point(11, 58);
+            this.gunaComboBox1.Name = "gunaComboBox1";
+            this.gunaComboBox1.OnHoverItemBaseColor = System.Drawing.Color.FromArgb(((int)(((byte)(100)))), ((int)(((byte)(88)))), ((int)(((byte)(255)))));
+            this.gunaComboBox1.OnHoverItemForeColor = System.Drawing.Color.White;
+            this.gunaComboBox1.Size = new System.Drawing.Size(133, 21);
+            this.gunaComboBox1.TabIndex = 5;
+            this.gunaComboBox1.SelectedIndexChanged += new System.EventHandler(this.gunaComboBox1_SelectedIndexChanged);
+            // 
+            // gunaComboBox2
+            // 
+            this.gunaComboBox2.BackColor = System.Drawing.Color.Transparent;
+            this.gunaComboBox2.BaseColor = System.Drawing.Color.FromArgb(((int)(((byte)(25)))), ((int)(((byte)(25)))), ((int)(((byte)(25)))));
+            this.gunaComboBox2.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(12)))), ((int)(((byte)(12)))), ((int)(((byte)(12)))));
+            this.gunaComboBox2.BorderSize = 1;
+            this.gunaComboBox2.DrawMode = System.Windows.Forms.DrawMode.OwnerDrawFixed;
+            this.gunaComboBox2.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.gunaComboBox2.FocusedColor = System.Drawing.Color.Empty;
+            this.gunaComboBox2.Font = new System.Drawing.Font("Segoe UI", 9F);
+            this.gunaComboBox2.ForeColor = System.Drawing.Color.White;
+            this.gunaComboBox2.FormattingEnabled = true;
+            this.gunaComboBox2.ItemHeight = 15;
+            this.gunaComboBox2.Items.AddRange(new object[] {
+            "Inbound Method",
+            "Outbound Method",
+            "LagSwich Method"});
+            this.gunaComboBox2.Location = new System.Drawing.Point(11, 85);
+            this.gunaComboBox2.Name = "gunaComboBox2";
+            this.gunaComboBox2.OnHoverItemBaseColor = System.Drawing.Color.FromArgb(((int)(((byte)(100)))), ((int)(((byte)(88)))), ((int)(((byte)(255)))));
+            this.gunaComboBox2.OnHoverItemForeColor = System.Drawing.Color.White;
+            this.gunaComboBox2.Size = new System.Drawing.Size(133, 21);
+            this.gunaComboBox2.TabIndex = 6;
+            this.gunaComboBox2.SelectedIndexChanged += new System.EventHandler(this.gunaComboBox2_SelectedIndexChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(32)))), ((int)(((byte)(32)))), ((int)(((byte)(32)))));
+            this.ClientSize = new System.Drawing.Size(155, 193);
+            this.Controls.Add(this.gunaComboBox2);
+            this.Controls.Add(this.gunaComboBox1);
+            this.Controls.Add(this.guna2CheckBox1);
+            this.Controls.Add(this.guna2Button2);
+            this.Controls.Add(this.guna2Button1);
+            this.Controls.Add(this.guna2TextBox1);
+            this.Controls.Add(this.gunaGradient2Panel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.gunaGradient2Panel1.ResumeLayout(false);
+            this.gunaGradient2Panel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Guna.UI2.WinForms.Guna2Elipse guna2Elipse1;
+        private Guna.UI2.WinForms.Guna2ImageButton guna2ImageButton1;
+        private Guna.UI.WinForms.GunaLabel gunaLabel1;
+        private Guna.UI2.WinForms.Guna2TextBox guna2TextBox1;
+        private Guna.UI2.WinForms.Guna2Button guna2Button1;
+        private Guna.UI2.WinForms.Guna2Button guna2Button2;
+        private Guna.UI2.WinForms.Guna2CheckBox guna2CheckBox1;
+        private Guna.UI.WinForms.GunaComboBox gunaComboBox2;
+        private Guna.UI.WinForms.GunaComboBox gunaComboBox1;
+        private Guna.UI.WinForms.GunaGradient2Panel gunaGradient2Panel1;
+    }
+}
+
---
+++ MisACK/Form1.cs
@@ -0,0 +1,305 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Media;
+using System.Reflection.Emit;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MisACK
+{
+    public partial class Form1 : Form
+    {
+        #region "Variables"
+        public static Point newpoint = new Point();
+        public static int x;
+        public static int y;
+
+        public Thread loop = new Thread(new ThreadStart(run))
+        {
+            IsBackground = true
+        };
+
+        public static int PressedKey;
+        public static string APP_PATH;
+        public static string LAG_TIME;
+        public static int LAG_METHOD;
+        public static bool LOOP_LAG;
+
+        public static SoundPlayer LAG_ON_AUDIO = new SoundPlayer(MisACK.Properties.Resources.on);
+        public static SoundPlayer LAG_OFF_AUDIO = new SoundPlayer(MisACK.Properties.Resources.off);
+
+        public static void sleep(int seconds) { System.Threading.Thread.Sleep(seconds); }
+        #endregion
+        #region "Virtual Key State"
+        [DllImport("user32.dll")]
+        static extern short GetAsyncKeyState(int VirtualKeyPressed);
+        #endregion
+        #region "Virutal Key"
+        public enum VKEY
+        {
+            VK_LBUTTON = 0x01, //Left mouse button
+            VK_RBUTTON = 0x02, //Right mouse button
+            VK_MBUTTON = 0x04, //Middle mouse button (three-button mouse)
+            VK_XBUTTON1 = 0x05, //X1 mouse button
+            VK_XBUTTON2 = 0x06, //X2 mouse button
+            VK_SHIFT = 0x10, //SHIFT key
+            VK_CONTROL = 0x11, //CTRL key
+            VK_MENU = 0x12, //ALT key
+            VK_A = 0x41, //A key
+            VK_F = 0x46, //F key
+            VK_F1 = 0x70, //F1 key
+            VK_F2 = 0x71, //F2 key
+            VK_F3 = 0x72, //F3 key
+        }
+        #endregion
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        #region "System Move Title Panel"
+        private void xMouseDown(object sender, MouseEventArgs e)
+        {
+            x = Control.MousePosition.X - base.Location.X;
+            y = Control.MousePosition.Y - base.Location.Y;
+        }
+        private void xMouseMove(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                newpoint = Control.MousePosition;
+                newpoint.X -= x;
+                newpoint.Y -= y;
+                base.Location = newpoint;
+            }
+        }
+        #endregion
+        #region "Button Exit"
+        private void guna2ImageButton1_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+        #endregion
+        #region "Form Load"
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            MessageBox.Show("Welcome to MisACK a simple program made by Misaki for make your lag on any games.", "MisACK", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            this.gunaGradient2Panel1.MouseDown += this.xMouseDown;
+            this.gunaGradient2Panel1.MouseMove += this.xMouseMove;
+            this.gunaGradient2Panel1.MouseDown += this.xMouseDown;
+            this.gunaGradient2Panel1.MouseMove += this.xMouseMove;
+
+            loop.Start();
+
+            gunaComboBox1.SelectedIndex = 1;
+            gunaComboBox2.SelectedIndex = 2;
+        }
+        #endregion
+        #region "Execute CMD"
+        public static string ExecuteCommand(string command)
+        {
+            ProcessStartInfo procStartInfo = new ProcessStartInfo("cmd", "/c " + command)
+            {
+                RedirectStandardError = true,
+                RedirectStandardOutput = true,
+                UseShellExecute = false,
+                CreateNoWindow = true
+            };
+
+            using (Process proc = new Process())
+            {
+                proc.StartInfo = procStartInfo;
+                proc.Start();
+
+                string output = proc.StandardOutput.ReadToEnd();
+
+                if (string.IsNullOrEmpty(output))
+                    output = proc.StandardError.ReadToEnd();
+
+                return output;
+            }
+        }
+
+        #endregion
+        #region "Select Program"
+        private void guna2Button2_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "Program (*.exe*)|*.exe*";
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                MessageBox.Show("Program found:\n\n" + openFileDialog.FileName, "MisACK", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                APP_PATH = openFileDialog.FileName;
+            }
+        }
+        #endregion
+        #region "Set lag time"
+        private void guna2Button1_Click(object sender, EventArgs e)
+        {
+            LAG_TIME = guna2TextBox1.Text;
+            MessageBox.Show("Timer has been set to: " + LAG_TIME, "MisACK", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+        #endregion
+        #region "Loop Function"
+        public async static void run()
+        {
+            while (true)
+            {
+                if (GetAsyncKeyState(Convert.ToInt32(PressedKey)) < 0)
+                {
+                    if (LAG_METHOD == 0)
+                    {
+                        LAG_ON_AUDIO.Play();
+                        ExecuteCommand("netsh advfirewall firewall add rule name =\"lagswitch\" dir=in action=block program=\"" + APP_PATH + "\"");
+                        sleep(Convert.ToInt32(LAG_TIME));
+                        ExecuteCommand("netsh advfirewall firewall delete rule name=\"lagswitch\"");
+                        LAG_OFF_AUDIO.Play();
+                    }
+                    else if (LAG_METHOD == 1)
+                    {
+                        LAG_ON_AUDIO.Play();
+                        ExecuteCommand("netsh advfirewall firewall add rule name =\"lagswitch\" dir=out action=block program=\"" + APP_PATH + "\"");
+                        sleep(Convert.ToInt32(LAG_TIME));
+                        ExecuteCommand("netsh advfirewall firewall delete rule name=\"lagswitch\"");
+                        LAG_OFF_AUDIO.Play();
+                    }
+                    else if (LAG_METHOD == 2)
+                    {
+                        LAG_ON_AUDIO.Play();
+                        ExecuteCommand("netsh advfirewall firewall add rule name =\"lagswitch\" dir=out action=block program=\"" + APP_PATH + "\"");
+                        ExecuteCommand("netsh advfirewall firewall add rule name =\"lagswitch\" dir=in action=block program=\"" + APP_PATH + "\"");
+                        sleep(Convert.ToInt32(LAG_TIME));
+                        ExecuteCommand("netsh advfirewall firewall delete rule name=\"lagswitch\"");
+                        LAG_OFF_AUDIO.Play();
+                    }
+                }
+                
+                if (LOOP_LAG)
+                {
+                    if (LAG_METHOD == 0)
+                    {
+                        LAG_ON_AUDIO.Play();
+                        ExecuteCommand("netsh advfirewall firewall add rule name =\"lagswitch\" dir=in action=block program=\"" + APP_PATH + "\"");
+                        sleep(Convert.ToInt32(LAG_TIME));
+                        ExecuteCommand("netsh advfirewall firewall delete rule name=\"lagswitch\"");
+                        LAG_OFF_AUDIO.Play();
+                    }
+                    else if (LAG_METHOD == 1)
+                    {
+                        LAG_ON_AUDIO.Play();
+                        ExecuteCommand("netsh advfirewall firewall add rule name =\"lagswitch\" dir=out action=block program=\"" + APP_PATH + "\"");
+                        sleep(Convert.ToInt32(LAG_TIME));
+                        ExecuteCommand("netsh advfirewall firewall delete rule name=\"lagswitch\"");
+                        LAG_OFF_AUDIO.Play();
+                    }
+                    else if (LAG_METHOD == 2)
+                    {
+                        LAG_ON_AUDIO.Play();
+                        ExecuteCommand("netsh advfirewall firewall add rule name =\"lagswitch\" dir=out action=block program=\"" + APP_PATH + "\"");
+                        ExecuteCommand("netsh advfirewall firewall add rule name =\"lagswitch\" dir=in action=block program=\"" + APP_PATH + "\"");
+                        sleep(Convert.ToInt32(LAG_TIME));
+                        ExecuteCommand("netsh advfirewall firewall delete rule name=\"lagswitch\"");
+                        LAG_OFF_AUDIO.Play();
+                    }
+                }
+            }
+        }
+        #endregion
+        #region "Start Loop lags"
+        private void guna2CheckBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            if (guna2CheckBox1.Checked)
+            {
+                LOOP_LAG = true;
+            }
+            else
+            {
+                LOOP_LAG = false;
+            }
+        }
+        #endregion
+        #region "Select key press"
+        private void gunaComboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (gunaComboBox1.SelectedIndex == 0)
+            {
+                PressedKey = 0x01;
+            }
+            else if (gunaComboBox1.SelectedIndex == 1)
+            {
+                PressedKey = 0x02;
+            }
+            else if (gunaComboBox1.SelectedIndex == 2)
+            {
+                PressedKey = 0x04;
+            }
+            else if (gunaComboBox1.SelectedIndex == 3)
+            {
+                PressedKey = 0x05;
+            }
+            else if (gunaComboBox1.SelectedIndex == 4)
+            {
+                PressedKey = 0x06;
+            }
+            else if (gunaComboBox1.SelectedIndex == 5)
+            {
+                PressedKey = 0x10;
+            }
+            else if (gunaComboBox1.SelectedIndex == 6)
+            {
+                PressedKey = 0x11;
+            }
+            else if (gunaComboBox1.SelectedIndex == 7)
+            {
+                PressedKey = 0x12;
+            }
+            else if (gunaComboBox1.SelectedIndex == 8)
+            {
+                PressedKey = 0x41;
+            }
+            else if (gunaComboBox1.SelectedIndex == 9)
+            {
+                PressedKey = 0x46;
+            }
+            else if (gunaComboBox1.SelectedIndex == 10)
+            {
+                PressedKey = 0x70;
+            }
+            else if (gunaComboBox1.SelectedIndex == 11)
+            {
+                PressedKey = 0x71;
+            }
+            else if (gunaComboBox1.SelectedIndex == 12)
+            {
+                PressedKey = 0x72;
+            }
+        }
+        #endregion
+        #region "Select lag methods"
+        private void gunaComboBox2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (gunaComboBox2.SelectedIndex == 0)
+            {
+                LAG_METHOD = 0;
+            }
+            else if (gunaComboBox2.SelectedIndex == 1)
+            {
+                LAG_METHOD = 1;
+            }
+            else if (gunaComboBox2.SelectedIndex == 2)
+            {
+                LAG_METHOD = 2;
+            }
+        }
+        #endregion
+    }
+}
